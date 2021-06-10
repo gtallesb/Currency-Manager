18eb44c17f60f12ec756aaf7c3ad2154487c8511
gtallesb <32268206+gtallesb@users.noreply.github.com>
2021-06-10 09:01:56 -0400

    Delete amountEntered.current.value = parseInt(a.js

--- src/amountEntered.current.value = parseInt(a.js
+++
@@ -1,7 +0,0 @@
-amountEntered.current.value = parseInt(amountEntered.current.value).toFixed(2);
-else amountEntered.current.value = parseInt(amountEntered.current.value);
-
-
-
-amountEntered.current.value = amountEnteredState + inputCode;
-
